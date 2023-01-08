bb3e324cf84de7584b540b704640322b1cc1e2b7
moDevsome <contact@mickael-outhier.fr>
2023-01-08 08:41:28 +0100

    Mise à jour de la page "A propos"

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -14,14 +14,20 @@ import DropdownPanComponent from '../components/utils/DropdownPanComponent';
 
 function AboutPage() {
 
-    const loremText = <article>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    const fiabilityText = <article>
+        <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
     </article>
 
     const respectText = <article>
         <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+    </article>
+
+    const serviceText = <article>
+        <p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>
+    </article>
+
+    const securityText = <article>
+        <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
     </article>
 
     return(
@@ -30,10 +36,10 @@ function AboutPage() {
                 <div id="About-inner">
                     <BannerComponent id={ 'About-banner' }/>
                     <section>
-                        <DropdownPanComponent title={ <h2>Fiabilité</h2> } content={ loremText } defaultState={ true }/>
+                        <DropdownPanComponent title={ <h2>Fiabilité</h2> } content={ fiabilityText } defaultState={ true }/>
                         <DropdownPanComponent title={ <h2>Respect</h2> } content={ respectText }/>
-                        <DropdownPanComponent title={ <h2>Service</h2> } content={ loremText } defaultState={ true }/>
-                        <DropdownPanComponent title={ <h2>Responsabilité</h2> } content={ loremText } defaultState={ true }/>
+                        <DropdownPanComponent title={ <h2>Service</h2> } content={ serviceText } defaultState={ true }/>
+                        <DropdownPanComponent title={ <h2>Responsabilité</h2> } content={ securityText } defaultState={ true }/>
                     </section>
                 </div>
             </div>
